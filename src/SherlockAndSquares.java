6d38d2b5a5c5f89f024fd046cd8e8c03096197da
dinhphucv <90453629+dinhphucv@users.noreply.github.com>
2025-02-10 04:47:32 -0800

    Update SherlockAndSquares.java

--- src/SherlockAndSquares.java
+++ src/SherlockAndSquares.java
@@ -1,2 +1,9 @@
 public class SherlockAndSquares {
+    public static void main(String[] args) {
+        System.out.println(squares(3, 9));
+    }
+
+    public static int squares(int a, int b) {
+        return ((int) Math.sqrt(b) - (int) Math.ceil(Math.sqrt(a))) + 1;
+    }
 }
